cd311f14726e18a7f829e4cd0106d3262bedf9de
tristanbrown <brown.tristan.r@gmail.com>
2020-01-20 00:32:02 -0600

    Ingest data

---
+++ __about__.py
@@ -0,0 +1,7 @@
+"""About deldup"""
+__name__ = 'deldup'
+__version__ = '0.0.1'
+__author__ = "Tristan R. Brown",
+__email__ = "brown.tristan.r@gmail.com",
+__desc__ = "An analysis package for characterizing genetic deletions and duplications.",
+__license__ = "MIT",
---
+++ src/deldup/__main__.py
@@ -0,0 +1,10 @@
+import argparse
+
+parser = argparse.ArgumentParser()
+parser.add_argument('path', help='Path to the input data.')
+
+def run():
+    pass
+
+if __name__ == "__main__":
+    run()
